0c53aa449b3ca65defdfc96da1b6144bf6ab1822
Sam Bulatov <mephistorine@gmail.com>
2023-02-19 12:41:06 +0300

    fix: Сортировка

--- src/app/pages/home-page/services/home-api.service.ts
+++ src/app/pages/home-page/services/home-api.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
-import { Observable } from "rxjs"
+import { map, Observable } from "rxjs"
 import { ConfigService } from "../../../global/services/config/config.service"
+import { sortAlphabet } from "../lib"
 import { BusinessCenter, RoomTelemetryInfo, StoreyTelemetryInfo } from "../models"
 
 @Injectable({
@@ -13,7 +14,34 @@ export class HomeApiService {
   }
 
   public getBusinessCenters(): Observable<readonly BusinessCenter[]> {
-    return this.httpClient.get<readonly BusinessCenter[]>(`${ this.configService.baseApiUrl }/business-center/all`)
+    return this.httpClient.get<readonly BusinessCenter[]>(`${ this.configService.baseApiUrl }/business-center/all`).pipe(
+      map((businessCenters: readonly BusinessCenter[]) => {
+        return businessCenters.map((center) => {
+          return {
+            ...center,
+            storeys: center.storeys
+              .map((storey) => {
+                return {
+                  ...storey,
+                  rooms: storey.rooms
+                    .slice()
+                    .sort((a, b) => {
+                      const [ , numeroA ] = a.name.split(" ")
+                      const [ , numeroB ] = b.name.split(" ")
+
+                      if (!numeroA || !numeroB) {
+                        return 1
+                      }
+
+                      return parseFloat(numeroA) - parseFloat(numeroB)
+                    })
+                }
+              })
+              .sort((a, b) => sortAlphabet(a.name, b.name))
+          }
+        })
+      })
+    )
   }
 
   public getRoomTelemetryInfo(id: number): Observable<RoomTelemetryInfo> {
